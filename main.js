5897e49511ff364d6f43478a8581fc04b98eb34d
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-03-21 20:10:26 -0300

    21.03

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-31T23:59:59");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
